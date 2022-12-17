bf8e8807b52964f7b050691b6abb8e5eb5753aad
Maria <you@example.com>
2022-12-18 00:56:20 +0300

    Сделаны общие стили почти для всех нужных нам элементов наметаны окно авторизации, глвное окно, страница клиента и страница списка клиентов

---
+++ View/Pages/ClientInfoPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CRMTelmate.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientInfoPage.xaml
+    /// </summary>
+    public partial class ClientInfoPage : Page
+    {
+        public ClientInfoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEditSrvc_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDeleteSrvc_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //открытие страницы определенного клиента 
+        //public ClientInfoPage(Entities.Client client)
+        //{
+        //}
+    }
+}
---
+++ View/Pages/ClientPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CRMTelmate.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientPage.xaml
+    /// </summary>
+    public partial class ClientPage : Page
+    {
+        public ClientPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            //это нерабочий примерный код навигации на страницу определенного клиента
+            //var button = sender as Button;
+            //var currentClient = button.DataContext as Entities.Clients;
+
+            //NavigationService.Navigate(new ClientInfoPage(currentClient));
+        }
+    }
+}
